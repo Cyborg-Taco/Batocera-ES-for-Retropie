df4e67ba34c72d7a7ee7e724fe9169989536e9e5
Cyborg-Taco <dudewithmenot@gmail.com>
2026-04-24 11:04:48 -0500

    fix updates to input configuration scripts and savestate config

--- Batocera-ES-for-Retropie.sh
+++ Batocera-ES-for-Retropie.sh
@@ -21,9 +21,10 @@ function _get_input_cfg_emulationstation() {
 }
 
 function _update_hook_Batocera-ES-for-Retropie() {
-    # make sure the input configuration scripts and launch script are always up to date
+    # make sure the input configuration scripts, savestate config, and launch script are always up to date
     if rp_isInstalled "$md_id"; then
         copy_inputscripts_emulationstation
+        install_savestates_emulationstation
         install_launch_emulationstation
     fi
 }
